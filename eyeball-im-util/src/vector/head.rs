faf18b25325e5bd7eda70597d6662111d9035465
Ivan Enderlin <ivan@mnt.io>
2025-01-15 17:41:09 +0100

    doc: Fix a typo.

--- eyeball-im-util/src/vector/head.rs
+++ eyeball-im-util/src/vector/head.rs
@@ -17,7 +17,7 @@ use pin_project_lite::pin_project;
 
 pin_project! {
     /// A [`VectorDiff`] stream adapter that presents a limited view of the
-    /// underlying [`ObservableVector`]s items. The view starts from index 0.
+    /// underlying [`ObservableVector`]'s items. The view starts from index 0.
     ///
     /// For example, let `S` be a `Stream<Item = VectorDiff>`. The [`Vector`]
     /// represented by `S` can have any length, but one may want to virtually
